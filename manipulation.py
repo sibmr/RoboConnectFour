60cd05d29ff2604fc62fa6ed4142937b82a42737
SimonsLab <simon.bihlmaier@gmail.com>
2020-05-27 17:16:12 +0200

    changes to make classes work outside of notebook

--- manipulation.py
+++ manipulation.py
@@ -1,17 +1,23 @@
 class Manipulation:
     
-    def __init__(self, tau):
+    def __init__(self, tau, C, V, S, ry):
+        self.C = C
+        self.V = V
+        self.S = S
+        self.ry = ry
+        
         self.tau = tau
         self.komo = C.komo_path(1.0, 1, self.tau, True)
         self.opening = False
         self.gripper_width_threshold = 1e-3 # TODO tbd
         
     def clear(self):
-        self.komo = C.komo_path(1.0, 1, self.tau, True)
+        self.komo = self.C.komo_path(1.0, 1, self.tau, True)
         self.komo.clearObjectives()
         self.komo.add_qControlObjective(order=1, scale=1)
     
     def grasp(self, gripper, obj):
+        ry = self.ry
         self.komo.addObjective([1.], ry.FS.positionDiff, [gripper,obj], ry.OT.sos, [1e3]);
         #self.komo.addObjective([1.], ry.FS.positionDiff, [gripper, obj], ry.OT.sos, [1e2]);
         self.komo.addObjective([1.], ry.FS.vectorZ, [gripper], ry.OT.sos, [1e2], target=[0,0,1]);
@@ -24,11 +30,12 @@ class Manipulation:
     def open_gripper(self):
         # TODO this is sketched code and needs work
         if not self.opening:
-            S.openGripper("R_gripper")
+            self.S.openGripper("R_gripper")
             self.opening = True
-        return S.getGripperWidth("R_gripper") < self.gripper_width_threshold
+        return self.S.getGripperWidth("R_gripper") < self.gripper_width_threshold
             
     def go_to_object(self, gripper, obj):
+        ry = self.ry
         # TODO make go_to_object() more general than grasp()
         self.komo.addObjective([1.], ry.FS.positionDiff, [gripper,obj], ry.OT.sos, [1e3]);
         #self.komo.addObjective([1.], ry.FS.positionDiff, ["R_gripperCenter", "percept"], ry.OT.sos, [1e2]);
--- robot.py
+++ robot.py
@@ -1,40 +1,48 @@
-class Robot():
+from manipulation import Manipulation
+import time
+import numpy as np
+
+class Robot(object):
     
-    def __init__(self, tau):
+    def __init__(self, tau, C, V, S, ry):
         self.tau = tau
         self.manipulation = Manipulation(tau=tau)
         self.is_closing = False
+        self.C = C
+        self.V = V
+        self.S = S
+        self.ry = ry
         
     def optimize(self):
         komo = self.manipulation.komo
         komo.optimize(True)
         end_config = komo.getConfiguration(0)
-        C.setFrameState(end_config)
-        V.setConfiguration(C)
-        q = C.getJointState()
+        self.C.setFrameState(end_config)
+        self.V.setConfiguration(self.C)
+        q = self.C.getJointState()
         return q
     
     def step(self, q=None):
         if q is None:
-            S.step([], self.tau, ry.ControlMode.none)
+            self.S.step([], self.tau, self.ry.ControlMode.none)
         else:
-            S.step(q, self.tau, ry.ControlMode.position)
+            self.S.step(q, self.tau, self.ry.ControlMode.position)
             
     def evaluate_dist(self, obj1, obj2):
-        [y,J] = C.evalFeature(ry.FS.positionDiff, [obj1, obj2])
+        [y,J] = self.C.evalFeature(self.ry.FS.positionDiff, [obj1, obj2])
         return y, J
     
     def grasp(self, gripper, obj):
         print("Grasp")
         # The high-level grasp task is only finished when the simulation yields getGripperIsGrasping == True
-        while not S.getGripperIsGrasping(gripper):
+        while not self.S.getGripperIsGrasping(gripper):
             time.sleep(self.tau)
-            q = S.get_q()
+            q = self.S.get_q()
             y, _ = self.evaluate_dist(gripper + "Center", obj)
             # TODO decide between np.linalg.norm(y) and np.abs(y).max()
             if not self.is_closing and np.linalg.norm(y) < 0.02: #np.abs(y).max() < 1e-2:
                 print("Closing Gripper")
-                S.closeGripper(gripper)
+                self.S.closeGripper(gripper)
                 self.is_closing = True # Avoid that closeGripper() is called multiple times
             self.manipulation.clear()  
             self.manipulation.grasp(gripper, obj)
@@ -49,7 +57,4 @@ class Robot():
         while not m.open():
             time.sleep(self.tau)
             self.step()
-
-robot = Robot(tau=0.01)
-robot.grasp("R_gripper", "percept")
 

b5b68543898f5712dd99ba4ea745d25c22bcb4a5
Thomas Monninger <t.monninger@t-online.de>
2020-07-08 21:30:19 +0200

    Outsource all perception methods into perception class, make class static

--- perception.py
+++ perception.py
@@ -2,51 +2,21 @@ import cv2 as cv
 import numpy as np
 
 class Perception(object):
-    # for segmentation
-    depth_background = None
-    rgb_background = None
-    rgb_background_blur = None
-    
-    blur = None
-    
-    # for ball exercise
-    vel = 0
-    prev_pos = None
-    
-    def __init__(self, f):
-        self.f = f
-        #self.blur = (lambda img : cv.GaussianBlur(img, (0,0), 0.6,0.6))
-       
-    def set_background(self,rgb_background, depth_background):
-        self.depth_background = depth_background
-        self.rgb_background = rgb_background
-        self.rgb_background_blur = self.blur(rgb_background)
-    
-    #def set_background_from_sim(self,sim):
-    #    self.set_background(*sim.getImageAndDepth())
-    
-    def get_object_pixels(self,depth):
-        cond = (self.depth_background > depth)
+
+    @staticmethod
+    def get_object_pixels(depth, depth_background):
+        cond = (depth_background > depth)
         imgray = (cond*255).astype(np.uint8)
         contours, hierarchy = cv.findContours(imgray,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
         cv.drawContours(rgb, contours, -1, (0,255,0), 3)
         return imgray
-    
-    def get_unique_colors(self):
-        return np.array(
-            [
-            [0,255,0]
-            [255,0,0],
-            [255,255,0],
-            [255,0,255],
-            [0,255,255],
-            ])
-    
-    def segment_rgb(self,rgb):
+
+    @staticmethod
+    def segment_rgb(rgb):
         opor = np.logical_or
         opand = np.logical_and
-        
-        diff = cv.absdiff(self.rgb_background_blur, self.blur(rgb))
+        rgb_blur = cv.GaussianBlur(rgb, (5,5), 1, 1)
+        diff = cv.absdiff(rgb_background_blur, rgb_blur)
 
         flatb = diff.reshape((360*640,3))[:,0]
         flatg = diff.reshape((360*640,3))[:,1]
@@ -62,18 +32,69 @@ class Perception(object):
         cv.drawContours(rgb, contours, -1, (0,255,255), 6)
 
         return rgb
-    
-    def segment_color(self, rgb, mask_rgb):
+
+    @staticmethod
+    def detect_grid_state(rgb, depth, display=True):
+        rgb = cv.rotate(rgb, cv.ROTATE_180)
+        depth = cv.rotate(depth, cv.ROTATE_180)
+        rgb = cv.GaussianBlur(rgb, (5,5), 1, 1)
+
+        grid_gray = Perception.extract_center_object(depth)
+        x,y,w,h = Perception.extract_rectangle(grid_gray)
+        rgb_grid = np.zeros(rgb.shape, dtype=np.uint8)
+        rgb_grid[y:y+h,x:x+w] = rgb[y:y+h,x:x+w]
+
+        center_lst_seg_red, center_lst_seg_blue = Perception.extract_circles(rgb_grid)
+
+        if display:
+            if len(rgb)>0:
+                cv.rectangle(rgb,(x,y),(x+w,y+h),(0,255,0),2)
+                cv.imshow('OPENCV - rgb', cv.cvtColor(rgb, cv.COLOR_RGB2BGR))
+            if len(rgb)>0:
+                Perception.draw_circles(rgb_grid, center_lst_seg_red, color=(255,0,0))
+                Perception.draw_circles(rgb_grid, center_lst_seg_blue, color= (0,0,255))
+                cv.imshow('OPENCV - rgb_grid', cv.cvtColor(rgb_grid, cv.COLOR_RGB2BGR))
+
+    @staticmethod
+    def extract_center_object(depth):
+        dim = depth.shape
+        depth_grid = depth[round(dim[0]/2), round(dim[1]/2)]
+        diff_bool = (np.abs(depth-depth_grid) < 1e-7)
+        diff_gray = diff_bool.astype(np.uint8) * 255
+        return diff_gray
+
+    @staticmethod
+    def extract_rectangle(img):
+        contours, hierarchy = cv.findContours(img, cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+        contour = max(contours, key = cv.contourArea)
+        x,y,w,h = cv.boundingRect(contour)
+        return x,y,w,h
+
+    @staticmethod
+    def extract_circles(img, display=False):
+        img_seg_red = Perception.segment_color(img, [255,0,0])
+        img_seg_blue = Perception.segment_color(img, [0,0,255])
+        if display:
+            if len(img_seg_red)>0:
+                cv.imshow('OPENCV - red', img_seg_red)
+            if len(img_seg_blue)>0:
+                cv.imshow('OPENCV - blue', img_seg_blue)
+        center_lst_seg_red = Perception.erode(img_seg_red)
+        center_lst_seg_blue = Perception.erode(img_seg_blue)
+        return center_lst_seg_red, center_lst_seg_blue
+
+    @staticmethod
+    def segment_color(rgb, mask_rgb):
         hsv = cv.cvtColor(rgb, cv.COLOR_RGB2HSV)
         mask_hsv = cv.cvtColor(np.uint8([[mask_rgb]]), cv.COLOR_RGB2HSV)[0,0]
         diff = np.array([30, 100, 100])
         lower = (mask_hsv - diff).clip(min=0)
         upper = (mask_hsv + diff).clip(max=255)
-        #imgray = (cv.inRange(hsv, lower, upper).astype(np.uint8))*255
         imgray = (cv.inRange(hsv, lower, upper))
         return imgray
 
-    def erode(self, imgray, iterations=5):
+    @staticmethod
+    def erode(imgray, iterations=5):
         #imgray = cv.GaussianBlur(imgray, (3,3), 1, 1)
         kernel = np.ones((3,3),np.uint8)
         imgray = cv.erode(imgray,kernel,iterations=iterations)
@@ -82,10 +103,10 @@ class Perception(object):
         for contour in contours:
             M = cv.moments(contour)
             center_lst.append([M["m10"] / M["m00"], M["m01"] / M["m00"], 10])
-        #rgb = cv.drawContours(rgb, contours, -1, (255,255,0), 3)
         return center_lst
 
-    def hough(self, imgray):
+    @staticmethod
+    def hough(imgray):
         circles = cv.HoughCircles(imgray,cv.HOUGH_GRADIENT,1,18,
                             param1=150,param2=10,minRadius=8,maxRadius=15)
         center_lst = []
@@ -93,43 +114,42 @@ class Perception(object):
             center_lst = circles[0]
         return center_lst
 
-    def draw_circles(self, img, circle_lst, color=(0,255,0)):
+    @staticmethod
+    def draw_circles(img, circle_lst, color=(0,255,0)):
         for i in circle_lst:
             cv.circle(img,(int(i[0]),int(i[1])),int(i[2]),color,2)
     
-    def get_image_coordinates_manual(self, depth, bin_img):
+    @staticmethod
+    def get_image_coordinates_manual(f, depth, bin_img):
         y, x = np.where(bin_img==255)
         z = depth[y,x]
-        x = (320-x.reshape((len(x),1)))/self.f
-        y = (180-y.reshape((len(y),1)))/self.f
+        x = (320-x.reshape((len(x),1))) / f
+        y = (180-y.reshape((len(y),1))) / f
         z = z.reshape((len(z),1))
         xyz = np.hstack((x,y,z))
         return xyz
     
-    def get_image_coordinates_auto(self, depth, bin_img, fxfypxpy, S):
+    @staticmethod
+    def get_image_coordinates_auto(depth, bin_img, fxfypxpy, S):
         x, y = np.where(bin_img==255)
         z = depth[x,y]
         x = x.reshape((len(x),1))
         y = y.reshape((len(y),1))
         z = z.reshape((len(z),1))
         points = S.depthData2pointCloud(depth, fxfypxpy)[x,y]
-        
         return points
     
-    def get_pc_mean(self, pc, prev_mean=None, use_velocity=0):
+    @staticmethod
+    def get_pc_mean(pc, vel, prev_mean=None, use_velocity=0):
         if prev_mean is not None:
-            diff = np.linalg.norm(pc-prev_mean, axis=2) 
-            
+            diff = np.linalg.norm(pc-prev_mean, axis=2)
             filter_outliers = np.where(diff<0.06)
-            
-            #print(pc.shape)
-            #print(filter_outliers)
             mean = np.mean(pc[filter_outliers],axis=0)
             
             if np.isnan(mean).any():
-                return prev_mean, prev_mean + self.vel*(use_velocity+1)
+                return prev_mean, prev_mean + vel*(use_velocity+1)
             else:
-                self.vel = prev_mean-mean
-                return mean, mean + self.vel*use_velocity
+                vel = prev_mean-mean
+                return mean, mean + vel*use_velocity
         else:
             return np.mean(pc,axis=0)[0]

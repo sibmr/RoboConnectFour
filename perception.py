72550a1a7b32b8b768aa96d964d5ede88894acbd
Thomas Monninger <t.monninger@t-online.de>
2020-07-09 15:20:54 +0200

    Combine methods into method extract_roi

--- perception.py
+++ perception.py
@@ -40,8 +40,7 @@ class Perception(object):
         depth = cv.rotate(depth, cv.ROTATE_180)
         rgb = cv.GaussianBlur(rgb, (5,5), 1, 1)
 
-        grid_gray = Perception.extract_roi(depth)
-        x,y,w,h = Perception.extract_rectangle(grid_gray)
+        x,y,w,h = Perception.extract_roi(depth)
         rgb_grid = np.zeros(rgb.shape, dtype=np.uint8)
         rgb_grid[y:y+h,x:x+w] = rgb[y:y+h,x:x+w]
 
@@ -93,11 +92,7 @@ class Perception(object):
         depth_grid = depth[round(dim[0]/2), round(dim[1]/2)]
         diff_bool = (np.abs(depth-depth_grid) < 1e-7)
         diff_gray = diff_bool.astype(np.uint8) * 255
-        return diff_gray
-
-    @staticmethod
-    def extract_rectangle(img):
-        contours, hierarchy = cv.findContours(img, cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv.findContours(diff_gray, cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
         contour = max(contours, key = cv.contourArea)
         x,y,w,h = cv.boundingRect(contour)
         return x,y,w,h

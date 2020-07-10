b56bbfac57c398c26e408c3f11a1310f1b278b10
Thomas Monninger <t.monninger@t-online.de>
2020-07-10 16:10:41 +0200

    Add selfstate option to game

--- game.py
+++ game.py
@@ -1,14 +1,19 @@
+from copy import deepcopy
+
 from grid import Grid
 
 
 class Game:
 
-    def __init__(self, strategy_1, strategy_2, grid=None):
+    def __init__(self, strategy_1, strategy_2, grid=None, selfstate=False):
         if grid is None:
             grid = Grid()
         self.grid = grid
-        self.player_1 = strategy_1(player=1, grid=self.grid)
-        self.player_2 = strategy_2(player=2, grid=self.grid)
+        self.selfstate = selfstate
+        self.grid_has_changed = False
+        self.next_move = True
+        self.player_1 = strategy_1(player=1, grid=self.grid, selfstate=selfstate)
+        self.player_2 = strategy_2(player=2, grid=self.grid, selfstate=selfstate)
         self.player = self.player_1
 
     def turn(self):
@@ -17,6 +22,17 @@ class Game:
         else:
             self.player = self.player_1
 
+    def set_grid(self, grid):
+        if not self.selfstate:
+            if not self.grid.equal(grid):
+                self.grid_has_changed = True
+            elif self.grid_has_changed:
+                self.grid_has_changed = False
+                self.next_move = True
+            self.grid.grid = grid
+        else:
+            print("Warning: Set grid but selfstate is True... Skip")
+
     def play(self):
         column = 0
         while column is not None:
@@ -24,6 +40,7 @@ class Game:
         self.grid.print()
 
     def step(self):
+        self.next_move = False
         self.grid.print()
         print("Player " + str(self.player.player) + "'s Turn: ", end='')
         if self.grid.won() is not None:
@@ -34,9 +51,9 @@ class Game:
         if column is None:
             print("*** Draw ***")
             return None
+
         self.turn()
         return column
-
 
 
 if __name__ == "__main__":
